7f6fef776cc1ab32aa1a3ca33c85847ebe90f5ed
Maria Zarechnaya <m.zarechnaya.m@mail.ru>
2024-01-30 11:44:43 +0300

    реализация функции создания и удаления карточки

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,24 @@
-// @todo: Темплейт карточки
+const card = document.querySelector("#card-template").content; // темплейт
+const list = document.querySelector(".places__list"); // место вставки карточек
 
-// @todo: DOM узлы
+// создание карточек из массива
+const createCard = function (array) {
+  array.forEach((item) => {
+    const cardElement = card.querySelector(".card").cloneNode(true); // для каждого элемента создаем шаблон карточки и наполняем его
+    const title = cardElement.querySelector(".card__title");
+    const image = cardElement.querySelector(".card__image");
+    title.textContent = item.name;
+    image.src = item.link;
+    cardElement
+      .querySelector(".card__delete-button")
+      .addEventListener("click", deleteCard); // навешиваем на каждую кнопку событие клика
+    list.append(cardElement);
+  });
+};
 
-// @todo: Функция создания карточки
+// удаление по кнопке
+function deleteCard(event) {
+  event.target.parentElement.remove();
+}
 
-// @todo: Функция удаления карточки
-
-// @todo: Вывести карточки на страницу
+createCard(initialCards);
